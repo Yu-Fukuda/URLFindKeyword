adcadc8d8e8c2c54954b6ccea56bbbe57620ed24
Yu-Fukuda <fukuda@advn.co.jp>
2016-06-24 14:41:48 +0900

    クラス分け決定

--- src/urlFindKeyword/SetURLKeyword.java
+++ src/urlFindKeyword/SetURLKeyword.java
@@ -3,6 +3,7 @@ package urlFindKeyword;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 
 public class SetURLKeyword {
     static final String setURL() throws IOException{
