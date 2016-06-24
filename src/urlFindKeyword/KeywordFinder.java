884815977385f65d00d414810c7abb50f5128d57
Yu-Fukuda <fukuda@advn.co.jp>
2016-06-24 14:41:23 +0900

    クラス分け決定

--- src/urlFindKeyword/KeywordFinder.java
+++ src/urlFindKeyword/KeywordFinder.java
@@ -1,8 +1,25 @@
 package urlFindKeyword;
 
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class KeywordFinder {
-    static Boolean keywordFound (){
+    static Boolean keywordFound () throws IOException{
         boolean mitsuketa = false;
+        
+        final String inputtedKeyword = "facebook"; //テスト用
+        Pattern pattern = Pattern.compile(inputtedKeyword);
+        
+        String httpSource = "oo";
+        
+         Matcher matcher = pattern.matcher(httpSource);
+         
+         if (matcher.find()) {            
+            mitsuketa = true;
+        } else {
+            mitsuketa = false;
+        }
         
         return mitsuketa;
         
